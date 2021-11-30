db53b2c18a6cb2689e7a75d65104df6e4761242b
KLan Stovall <kland13@gmail.com>
2021-11-29 19:51:18 -0800

    lab01 complete

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,6 +1,6 @@
 import sys
 
-menu="""**************************************
+menu = """**************************************
 **    Welcome to the Snakes Cafe!   **
 **    Please see our menu below.    **
 **
@@ -36,11 +36,20 @@ Unicorn Tears
 ** What would you like to order? **
 ***********************************"""
 print(menu)
-orderedItem = input(">")
-order = 0
-while True:
-        order += 1
-        print(f"** {order} order of {orderedItem} have been added to your meal**")
+
+def orderStuff():
+    meal = []
+    while True:
         orderedItem = input(">")
         if orderedItem.lower() == "quit":
+            print(f"You ordered: {meal}")
             sys.exit()
+        elif orderedItem not in meal:
+            print(f"** 1 order of {orderedItem} have been added to your meal **")
+            meal.append(orderedItem)
+        elif orderedItem in meal:
+            order = meal.count(orderedItem) + 1
+            print(f"** {order} orders of {orderedItem} have been added to your meal **")
+            meal.append(orderedItem)
+
+orderStuff()
